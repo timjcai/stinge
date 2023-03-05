873d4e4b16c075d63522347b7ca4a93c22a8efca
Tim Cai <tim.j.cai@gmail.com>
2023-03-06 09:09:26 +1100

    created 7 CRUD actions for shoppinglist

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   get 'batch', to: 'batch#index', as: 'batches'
   get 'batch/:id', to: 'batch#show', as: 'batch'
   get 'shoppinglist', to: 'shoppinglist#index', as: 'shoppinglist_history'
-  get 'shoppinglist/:id', to: 'shoppinglist#show', as: 'shoppinglists'
+  get 'shoppinglist/new', to: 'shoppinglist#new', as: 'new_shoppinglist'
+  get 'shoppinglist/:id', to: 'shoppinglist#show', as: 'shoppinglist'
+  post 'shoppinglist', to: 'shoppinglist#create'
+  get 'shoppinglist/:id/edit', to: 'shoppinglist#edit', as: 'edit_shoppinglist'
+  patch 'shoppinglist/:id', to: 'shoppinglist#update', as: 'update_shoppinglist'
+  delete 'shoppinglist/:id', to: 'shoppinglist#destroy', as: 'delete_shoppinglist'
 end
