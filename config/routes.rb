61806f9dd988c852d2e92e5aa0143cc2a710ad56
Tim Cai <tim.j.cai@gmail.com>
2023-03-08 15:26:13 +1100

    create navigation for current list

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'product/:id', to: 'product#show', as: 'product'
   get 'list', to: 'list#index', as: 'lists'
   get 'list/new', to: 'list#new', as: 'new_list'
+  get 'list/current', to: 'list#current', as: 'current_list'
   get 'list/:id', to: 'list#show', as: 'list'
 
   post 'list', to: 'list#create'
